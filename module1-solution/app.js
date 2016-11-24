f3dc188ff7785c257dc0c88032b6e9b8af652f3e
Siri <Siri>
2016-11-24 21:07:03 +0530

    null condition handled

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,20 +1,22 @@
 (function () {
   'use strict';
-var mod =  angular.module('LunchCheck',[]);
+var mod =  angular.module('myFirstApp',[]);
 mod.controller.$injector=['$scope'];
-mod.controller('LunchCheckController',function ($scope) {
+mod.controller('myFirstController',function ($scope) {
   $scope.name="";
 
   $scope.splitter = function () {
     var arrayOfStrings;
-
-    $scope.inputList = $scope.inputList.trim();
-    arrayOfStrings = $scope.inputList.split(",");
-    arrayOfStrings= arrayOfStrings.filter(function (value) {
+    if($scope.inputList){
+    	$scope.inputList = $scope.inputList.trim();
+    	arrayOfStrings = $scope.inputList.split(",");
+    	arrayOfStrings= arrayOfStrings.filter(function (value) {
          console.log(value!="");
          return value !="";
-    });
-    if(arrayOfStrings.length == 0 ){
+    	});
+    }
+
+    if(arrayOfStrings!=null && arrayOfStrings.length > 0 ){
         if(arrayOfStrings.length<=3){
             $scope.name="Enjoy!";
             console.log('Enjoy!');
@@ -29,11 +31,5 @@ mod.controller('LunchCheckController',function ($scope) {
   };
 });
 
-// mod.controller('LunchCheckController',function ($scope,$filter) {
-//     $scope.name="ravi kumar";
-//
-//     $scope.upCase=function () {
-//       $scope.name=$filter('uppercase')($scope.name);
-//     };
-//   });
+
 })();
